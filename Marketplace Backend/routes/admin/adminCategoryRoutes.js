7fde10058ca13465a4fbc34dc66c950ec80ba018
ijjaasss <ijasmuthu04@gmail.com>
2026-07-14 17:31:16 +0530

    change a file

--- Marketplace Backend/routes/admin/adminCategoryRoutes.js
+++ Marketplace Backend/routes/admin/adminCategoryRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { adminAuthMiddleware, isAdmin } from "../../middlewares/adminMiddleware.js";
-import { getCategories, getCategoryById, toggleCategoryStatus, updateCategory } from "../../controllers/admin/categoriesController.js";
+import { createCategory, getCategories, getCategoryById, toggleCategoryStatus, updateCategory } from "../../controllers/admin/categoriesController.js";
 import upload from "../../middlewares/upload.js";
 
 
@@ -9,6 +9,7 @@ const router=express.Router()
 
 router.use(adminAuthMiddleware,isAdmin);
 router.get('/',getCategories)
+router.post('/',createCategory)
 router.get('/:id',getCategoryById)
 router.put("/:id",upload.single("image"),updateCategory)
 router.patch("/:id/status",toggleCategoryStatus)
